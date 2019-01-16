210a55e3eeb58b150460883062ecdbcfa2a64f1d
Sam Lee <samlee@Sams-MacBook-Pro.local>
2019-01-16 19:30:52 +0900

    chunk type updated & pay function trimming.

--- utxo.go
+++ utxo.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/key-inside/kiesnet-ccpkg/contract"
 	"github.com/key-inside/kiesnet-ccpkg/txtime"
 )
 
@@ -17,28 +16,18 @@ const (
 
 // PayChunk _
 type PayChunk struct {
-	DOCTYPEID   string       `json:"@pay_chunk"` // id
-	Type        PayChunkType `json:"type"`
+	DOCTYPEID   string       `json:"@chunk"`  // id
 	Account     string       `json:"account"` // account ID (address)
-	RID         string       `json:"rid"`     // relative ID - account or contract
 	Amount      Amount       `json:"amount"`
-	Memo        string       `json:"memo"`
 	CreatedTime *txtime.Time `json:"created_time,omitempty"`
 }
 
 // NewPayChunkType _
-func NewPayChunkType(id string, owner Identifiable, rel Identifiable, amount Amount, memo string, cTime *txtime.Time) *PayChunk {
-	ptype := PayChunkTypeAccount
-	if _, ok := rel.(*contract.Contract); ok {
-		ptype = PayChunkTypeContract
-	}
+func NewPayChunkType(id string, owner Identifiable, amount Amount, cTime *txtime.Time) *PayChunk {
 	return &PayChunk{
 		DOCTYPEID:   id,
-		Type:        ptype,
 		Account:     owner.GetID(),
-		RID:         rel.GetID(),
 		Amount:      amount,
-		Memo:        memo,
 		CreatedTime: cTime,
 	}
 }
