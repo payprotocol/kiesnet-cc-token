0f884c2a77e743bf74ef1403237d6567047885f6
Jason <js.kim@key-inside.com>
2019-01-28 12:16:18 +0900

    pay-refund

--- pay.go
+++ pay.go
@@ -8,29 +8,19 @@ import (
 
 // ???: 전체적인 naming 문제
 
-// PayPayType _
-type PayPayType int8
-
-// ???: useless
-const (
-	// PayPayTypeAccount _
-	PayPayTypeAccount PayPayType = iota
-	// PayPayTypeContract _
-	PayPayTypeContract
-)
-
 // Pay _
 type Pay struct {
-	DOCTYPEID   string       `json:"@pay"`           // pay id
-	Owner       string       `json:"owner"`          // owner id
-	Amount      Amount       `json:"amount"`         //can be positive(pay) or negative(refund)
-	RID         string       `json:"rid"`            //related id. user who pays to the merchant or receives refund from the merchant.
-	PKey        string       `json:"pkey,omitempty"` //parent key. this value exists only when the pay type is refund(negative amount)
+	DOCTYPEID   string       `json:"@pay"`                   // pay id
+	Account     string       `json:"account"`                // owner id
+	Amount      Amount       `json:"amount"`                 //can be positive(pay) or negative(refund)
+	TotalRefund Amount       `json:"total_refund,omitempty"` // Total refund value
+	RID         string       `json:"rid"`                    //related id. user who pays to the merchant or receives refund from the merchant.
+	ParentKey   string       `json:"parent_key,omitempty"`   //parent key. this value exists only when the pay type is refund(negative amount)
 	CreatedTime *txtime.Time `json:"created_time,omitempty"`
 }
 
 // NewPayType _
-func NewPayType(key, owner string, amount Amount, rid, pkey string, ts *txtime.Time) *Pay {
+func NewPay(key, account string, amount Amount, rid, pkey string, ts *txtime.Time) *Pay {
 	return &Pay{
 		DOCTYPEID:   key,
 		Owner:       owner,
